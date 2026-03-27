88bdc7ef50a180c871ba22c6773322690ecf63be
oscarsidsa81 <ovilches@sid-sa.com>
2026-03-27 12:36:17 +0100

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'sid_block_3_views',
+    'summary': 'Metamodule sid_block_3_views',
+    'version': '15.0.1.0.0',
+    'category': 'Hidden',
+    'author': 'SIDSA',
+    'license': 'LGPL-3',
+    'depends': ['sid_block_2_sales_purchase', 'sid_sale_line_views_ov', 'sid_purchase_views_ov'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
